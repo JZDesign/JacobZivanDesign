330965efcbf562ef205a23a7443b8e4439f97239
Jacob Rakidzich <jacob.rakidzich@daveramsey.com>
2022-05-17 06:56:35 -0500

    finish updating dependencies

--- JZD_Publish/Package.swift
+++ JZD_Publish/Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/JZDesign/CNAMEPublishPlugin", branch: "hotfix.support.macos.v12"),
         .package(url: "https://github.com/johnsundell/publish.git", .upToNextMajor(from: "0.9.0")),
-        .package(url: "https://github.com/JZDesign/splash", branch: "hotfix.support.macos.v12")
+        .package(url: "https://github.com/johnsundell/splashpublishplugin",.upToNextMajor(from: "0.2.0"))
 
     ],
     targets: [
@@ -22,7 +22,7 @@ let package = Package(
             name: "JZDPublish",
             dependencies: [
                 .product(name: "Publish", package: "publish"),
-                .product(name: "SplashPublishPlugin", package: "splash"),
+                .product(name: "SplashPublishPlugin", package: "splashpublishplugin"),
                 .product(name: "CNAMEPublishPlugin", package: "CNAMEPublishPlugin")
             ]
         )
