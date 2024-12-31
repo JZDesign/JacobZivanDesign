0073f05d3f744d69f6a1e388afb0b8f666cfc7a3
JZDesign <jacob@jacobzivandesign.com>
2024-12-31 13:09:55 -0600

    update about, and add donate stuff.

--- JZD_Publish/Content/about.md
+++ JZD_Publish/Content/about.md
@@ -1,3 +1,20 @@
-# About Me
+# Jacob Zivan Design
 
-Hey
+<br/>
+
+Hey!
+
+My name is Jacob. I'm a follower of Jesus, a father, and a nerd. About that nerd part—which this site is mostly focused on—I'm a staff software engineer who's passionate about the Swift and Kotlin languages. I've been fortunate enough to be mentored by some of the brightest and kindest people I've ever met. They are certainly a major part of why I've made it to where I'm at, and I'm very grateful.
+
+I've been able to work on some great applications like:
+
+- [Everydollar](https://apps.apple.com/us/app/everydollar-personal-budget/id942571931) — _both iOS and Android before it was rewritten in Xamarin due to some C# fixated leaders_
+- [Kroger](https://apps.apple.com/us/developer/the-kroger-co/id403901189?see-all=i-phonei-pad-apps) — _technically all of their umbrella's mobile shopping apps_
+- [Gazelle](https://www.ramseysolutions.com/gazelle) — A Fintech app that didn't last
+- And others that are private enterprise apps
+
+<br/>
+<br/>
+
+For any questions or feedback, you can email Help@JacobZivanDesign.com and I'll get back to you when I can.
+
--- JZD_Publish/Sources/JZDPublish/Theme/Footer.swift
+++ JZD_Publish/Sources/JZDPublish/Theme/Footer.swift
@@ -6,13 +6,20 @@ extension Node where Context == HTML.BodyContext {
     
     static func footer<T: Website>(for site: T) -> Node {
         return .footer(
-            .p(
-                .text("Generated using "),
-                .a(
-                    .text("Publish"),
-                    .href("https://github.com/johnsundell/publish")
-                )
+            .br(),
+            .raw("""
+<div class="center">
+    <p><sm>
+    If you enjoy the content on my site and want to support my writing, you can donate <a href="https://ko-fi.com/jacobzivandesign" target="blank">here</a> or <a href="https://github.com/sponsors/JZDesign" target="blank">here.</a>
+    </p></sm>
+    <p><sm>
+    Or if you're a <a href="https://wallet.proton.me" target="blank">Proton Wallet</a> user, you can send coin to donate@jacobzivandesign.com
+    </p></sm>
+</div>
+"""
             ),
+            .br(),
+            .br(),
             .p(
                 .text("Code typeface == "),
                 .a(
@@ -21,15 +28,26 @@ extension Node where Context == HTML.BodyContext {
                 ),
                 .class("jbmono")
             ),
+            .p(
+                .text("Generated using "),
+                .a(
+                    .text("Publish"),
+                    .href("https://github.com/johnsundell/publish")
+                )
+            ),
+            
             .p(.a(
                 .text("RSS feed"),
                 .href("/feed.rss")
             )),
+            
             .div(
                 .a(.img(.src("/images/git.svg")), .target(.blank), .class("find-me"), .href("https://github.com/JZDesign/")),
                 .a(.img(.src("/images/linkedin.svg")), .target(.blank), .class("find-me"), .href("https://www.linkedin.com/in/jacob-rakidzich-b8722436/")),
                 .class("social")
-            )
+            ),
+            .br()
+            
         )
     }
 }
